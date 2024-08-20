9b29e559fdacf7b1017f7ad76c8f3d09feac8713
anyars-encarta <anyarsencarta@gmail.com>
2024-08-20 11:28:26 +0000

    Creat bank cards

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import BankCard from './BankCard'
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
     return (
@@ -36,6 +37,29 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
                         <h2 className='text-14 font-semibold text-gray-600'>Add Bank</h2>
                     </Link>
                 </div>
+
+                {banks?.length > 0 && (
+                    <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
+                        <div className='relative z-10'>
+                            <BankCard
+                                key={banks[0].$id}
+                                account={banks[0]}
+                                userName={`${user.firstName} ${user.lastName}`}
+                                showBalance={false}
+                            />
+                        </div>
+                        {banks[1] && (
+                            <div className='absolute right-0 top-8 z-0 w-[90%]'>
+                                <BankCard
+                                    key={banks[1].$id}
+                                    account={banks[1]}
+                                    userName={`${user.firstName} ${user.lastName}`}
+                                    showBalance={false}
+                                />
+                            </div>
+                        )}
+                    </div>
+                )}
             </section>
         </aside>
     )
