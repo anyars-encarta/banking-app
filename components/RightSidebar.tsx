9ef9f3a4b32a8bdc790d14ab867580dcfe015f69
anyars-encarta <anyarsencarta@gmail.com>
2024-08-20 10:14:38 +0000

    Add bank header

--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,3 +1,5 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
@@ -17,6 +19,22 @@ const RightSidebar = ({ user, transactions, banks }: RightSidebarProps) => {
 
                         <p className='profile-email'>{user.email}</p>
                     </div>
+                </div>
+            </section>
+
+            <section className='banks'>
+                <div className='flex w-full justify-between'>
+                    <h2 className='header-2'>My Banks</h2>
+                    <Link href='/' className='flex gap-2'>
+                        <Image
+                            src='/icons/plus.svg'
+                            width={20}
+                            height={20}
+                            alt='plus'
+                        />
+
+                        <h2 className='text-14 font-semibold text-gray-600'>Add Bank</h2>
+                    </Link>
                 </div>
             </section>
         </aside>
