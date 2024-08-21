d06611bef9a8d669b26eeb42ee7472318cba779b
anyars-encarta <anyarsencarta@gmail.com>
2024-08-21 17:56:42 +0000

    Fetch and display dynamic user data in home page and sidebar

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,10 +1,6 @@
 import AuthForm from '@/components/AuthForm'
-// import { getLoggedInUser } from '@/lib/actions/user.actions';
 
 const SignUp = async () => {
-  // const loggedInUser = await getLoggedInUser();
-
-  // console.log(loggedInUser)
 
   return (
     <section className='flex-center size-full max-sm:px-6'>
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,10 @@
 import HeaderBox from '@/components/HeaderBox'
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
+import { getLoggedInUser } from '@/lib/actions/user.actions';
 
-const Home = () => {
-  const loggedIn = {
-    firstName: 'Anyars',
-    lastName: 'Yussif',
-    email: 'anyars@something.com',
-  };
+const Home = async () => {
+  const loggedIn = await getLoggedInUser();
 
   return (
     <section className='home'>
@@ -16,7 +13,7 @@ const Home = () => {
           <HeaderBox
             type='greeting'
             title='Welcome'
-            user={loggedIn?.firstName || 'guest'}
+            user={loggedIn?.name || 'guest'}
             subtext='Access & manage your account and transactions efficiently.'
           />
 
