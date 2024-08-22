5343414d25e0d1d6538cbac45d450a467c517a25
anyars-encarta <anyarsencarta@gmail.com>
2024-08-22 08:20:45 +0000

    Correct sign-in action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,48 @@
 'use server';
 
-import { ID } from "node-appwrite";
+import { ID, Query } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
+
+const {
+    APPWRITE_DATABASE_ID: DATABASE_ID,
+    APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+    APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+  } = process.env;
+
+export const getUserInfo = async ({ userId }: getUserInfoProps) => {
+    try {
+      const { database } = await createAdminClient();
+  
+      const user = await database.listDocuments(
+        DATABASE_ID!,
+        USER_COLLECTION_ID!,
+        [Query.equal('userId', [userId])]
+      )
+  
+      return parseStringify(user.documents[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
 export const signIn = async ({ email, password }: signInProps) => {
     try {
         const { account } = await createAdminClient();
 
-        const response = await account.createEmailPasswordSession(email, password)
+        const session = await account.createEmailPasswordSession(email, password);
 
-        return parseStringify(response)
+        cookies().set('appwrite-session', session.secret, {
+            path: '/',
+            httpOnly: true,
+            sameSite: 'strict',
+            secure: true,
+        });
+
+        const user = await getUserInfo({ userId: session.userId})
+
+        return parseStringify(user)
     } catch (e) {
         console.log(e)
     }
@@ -59,10 +90,10 @@ export async function getLoggedInUser() {
 export const logoutAccount = async () => {
     try {
         const { account } = await createSessionClient();
-        cookies().delete('appwrite-session')
+        cookies().delete('appwrite-session');
 
-        await account.deleteSession('current')
+        await account.deleteSession('current');
     } catch (e) {
-        return null
+        return null;
     }
 }
