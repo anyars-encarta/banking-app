00de10148ca38718225daa69d39ad3e58a3843d9
anyars-encarta <anyarsencarta@gmail.com>
2024-08-27 11:05:46 +0000

    Install dwolla v2 and add dwolla actions

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -13,6 +13,7 @@ import { authFormSchema } from '@/lib/utils';
 import SpinnerLoader from './SpinnerLoader';
 import { useRouter } from 'next/navigation';
 import { getLoggedInUser, signIn, signUp } from '@/lib/actions/user.actions';
+import PlaidLink from './PlaidLink';
 
 const AuthForm = ({ type }: { type: string }) => {
     const router = useRouter();
@@ -90,11 +91,11 @@ const AuthForm = ({ type }: { type: string }) => {
                 </div>
             </header>
 
-            {user ? (
+            {/* {user ? ( */}
                 <div className='flex flex-col gap-4'>
-                    {/* PlaidLink */}
+                    <PlaidLink user={user} variant='primary' />
                 </div>
-            ) : (
+             {/* ) : ( */}
                 <>
                     <Form {...form}>
                         <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
@@ -216,7 +217,7 @@ const AuthForm = ({ type }: { type: string }) => {
                         </Link>
                     </footer>
                 </>
-            )}
+            {/* )} */}
         </section>
     )
 }
