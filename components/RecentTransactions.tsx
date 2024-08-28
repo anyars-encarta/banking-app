501b36978fa7abff6347355d99120f55f8cd42b4
anyars-encarta <anyarsencarta@gmail.com>
2024-08-28 15:40:57 +0000

    Add tabs and bank info components

--- components/RecentTransactions.tsx
+++ components/RecentTransactions.tsx
@@ -1,5 +1,9 @@
 import Link from 'next/link'
-import React from 'react'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import { BankTabItem } from './BankTabItem'
+import BankInfo from './BankInfo'
+import TransactionsTable from './TransactionsTable'
+
 
 const RecentTransactions = ({
   accounts,
@@ -7,6 +11,7 @@ const RecentTransactions = ({
   appwriteItemId,
   page = 1
 }: RecentTransactionsProps) => {
+  console.log(accounts)
   return (
     <section className='recent-transactions'>
       <header className='flex items-center justify-between'>
@@ -17,7 +22,33 @@ const RecentTransactions = ({
         </Link>
       </header>
 
-      
+      <Tabs defaultValue={appwriteItemId} className="w-full">
+        <TabsList className='recent-transactions-tablist'>
+          {accounts.map((account: Account) => (
+            <TabsTrigger value={account.appwriteItemId} key={account.id}>
+              <BankTabItem
+                key={account.id}
+                account={account}
+                appwriteItemId={appwriteItemId}
+              />
+            </TabsTrigger>
+          ))}
+        </TabsList>
+
+        {accounts.map((account: Account) => (
+          <TabsContent value={account.appwriteItemId} key={account.id} className='space-y-4'>
+            <BankInfo
+              account={account}
+              appwriteItemId={appwriteItemId}
+              type='full'
+            />
+
+            <TransactionsTable
+              transactions={transactions}
+            />
+          </TabsContent>
+        ))}
+      </Tabs>
     </section>
   )
 }
---
+++ components/TransactionsTable.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransactionsTable = (transactions: TransactionTableProps) => {
+  return (
+    <div>TransactionsTable</div>
+  )
+}
+
+export default TransactionsTable
